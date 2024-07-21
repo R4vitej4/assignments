20f512c0ca213a7b907d8546050939e9ad6e62f2
Raviteja <pegadaraviteja225@gmail.com>
2024-07-21 22:01:38 +0530

    initial commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  let n1=str1.length;
+  let n2=str2.length;
+  if(n1!=n2){
+    return false;
+  }
+  let freq={};
+  for (let char of str1){
+    freq[char]=(freq[char]||0)+1;
+  }
 
+  for(let char of str2){
+    if(!freq[char]){
+      return false;
+    }
+    freq[char]-=1;
+  }
+
+  for(let key in freq){
+    if(freq[key]!=0)return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
