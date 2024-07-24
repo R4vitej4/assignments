ecc05bd0b3bbb640ccd59fffbb5d1ba2e1b24199
Raviteja <pegadaraviteja225@gmail.com>
2024-07-24 21:51:34 +0530

    promises

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,15 @@
  */
 
 function sleep(milliseconds) {
+    return new Promise((resolve)=>{
+        const start = new Date().getTime();
+        // This loop continues until the current time minus the start time is greater than the number of milliseconds passed during this time the thread should not be able to do anything else.
+        // This is a busy wait.
+        while (new Date().getTime() - start < milliseconds) {
+            // Do nothing
+        }
+        resolve();
+    })
 }
 
 module.exports = sleep;
